a9959cc525b3df00c087c755a7ac27daae6be0ec
Francisco Gomes <fgsj.dev@outlook.com>
2022-04-19 13:43:11 -0300

    números ímpares

---
+++ src/StructureRepetition/exercise07.java
@@ -0,0 +1,14 @@
+package StructureRepetition;
+
+public class exercise07 {
+    public static void main(String[] args) {
+        //Section of variables
+
+        //Logical operations
+        for (int x=0; x < 50;x++){
+            if (x % 2 == 1){
+                System.out.println(x);
+            }
+        }
+    }
+}
